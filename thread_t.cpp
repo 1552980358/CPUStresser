53d4f5c6c2b9d839b33db017b27844ed00e97497
1552980358 <1552980358@qq.com>
2022-06-17 19:07:19 +0800

    thread_t.cpp: Implement AVX computing

--- thread_t.cpp
+++ thread_t.cpp
@@ -4,9 +4,35 @@
 using std::cout;
 using std::endl;
 
+#include <immintrin.h>
+
+void avx256_double_div_infinitive(double *result, const __m256d &format, int64_t &num) {
+    __m256d result_div_infinitive = _mm256_div_pd(format, _mm256_set_pd((double) num, (double) (num + 2), (double) (num + 4), (double) (num + 6)));
+    auto * result_div = (double *) &result_div_infinitive;
+    for (int i = 0; i < 4; ++i) {
+        result[i] = result_div[i];
+    }
+    num += 8;
+}
+
+double avx128_double_sum(double *result_avx256) {
+    __m128d result_avx128 = _mm_add_pd(
+            _mm_set_pd(result_avx256[0], result_avx256[1]),
+            _mm_set_pd(result_avx256[2], result_avx256[3])
+            );
+    auto result = (double *) &result_avx128;
+    return result[0] + result[1];
+}
+
 void thread_runnable(thread_t *thread_ptr, const int &id) {
     thread_ptr->started();
+    auto format = _mm256_set_pd((double) 4, (double) -4, (double) 4, (double) -4);
+    int64_t num = 1;
+    double pi = 0;
+    double buffer[4];
     while (thread_ptr->is_run()) {
+        avx256_double_div_infinitive((double *) &buffer, format, num);
+        pi += avx128_double_sum((double *) &buffer);
     }
 }
 
