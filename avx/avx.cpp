fa4ca6fa2e7adc55c3cfc9084fa8d6c3993f2fab
1552980358 <1552980358@qq.com>
2022-06-18 12:05:02 +0800

    avx.cpp: Update and implement methods assign_input and double_add_result

--- avx/avx.cpp
+++ avx/avx.cpp
@@ -3,13 +3,14 @@
 #include "avx256.h"
 #include "avx512.h"
 
+void assign_input(double *, int64_t &, const int &);
+
 void copy_result_to_input(double *, const double *, const int &);
 
+double double_add_result(const double *);
+
 double avx512_pi_mix(double *buffer_input, double *buffer_result, int64_t &num) {
-    for (int i = 0; i < 8; ++i) {
-        buffer_input[i] = (i % 2) ? -4 : 4;
-        buffer_result[i + 8] = (double) (num += 2);
-    }
+    assign_input(buffer_input, num, 8);
 
     avx512_div_double(buffer_input, buffer_result);
 
@@ -19,25 +20,33 @@ double avx512_pi_mix(double *buffer_input, double *buffer_result, int64_t &num) 
     copy_result_to_input(buffer_input, buffer_result, 4);
     avx128_add_double(buffer_input, buffer_result);
 
-    return buffer_result[0] + buffer_result[1];
+    return double_add_result(buffer_result);
 }
 
 double avx256_pi_mix(double *buffer_input, double *buffer_result, int64_t &num) {
-    for (int i = 0; i < 4; ++i) {
-        buffer_input[i] = (i % 2) ? -4 : 4;
-        buffer_result[i + 4] = (double) (num += 2);
-    }
+    assign_input(buffer_input, num, 4);
 
     avx256_div_double(buffer_input, buffer_result);
 
     copy_result_to_input(buffer_input, buffer_result, 4);
     avx128_add_double(buffer_input, buffer_result);
 
-    return buffer_result[0] + buffer_result[1];
+    return double_add_result(buffer_result);
+}
+
+void assign_input(double *buffer_input, int64_t &num, const int &repeat) {
+    for (int i = 0; i < repeat; ++i) {
+        buffer_input[i] = (i % 2) ? -4 : 4;
+        buffer_input[i + repeat] = (double) (num += 2);
+    }
 }
 
 void copy_result_to_input(double *buffer_input, const double *buffer_result, const int &result_size) {
     for (int i = 0; i < 8; ++i) {
         buffer_input[i] = buffer_result[i];
     }
+}
+
+double double_add_result(const double *buffer_result) {
+    return buffer_result[0] + buffer_result[1];
 }
