9be109a1625051c0f921056819d402c0dc3c20ae
1552980358 <1552980358@qq.com>
2022-10-11 20:49:54 +0800

    main.cpp: Implement environment module

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+using std::cout;
+using std::endl;
 
 #include "argument_parameters.h"
 #include "exit_code.h"
 
+#include "environment/environment.h"
+
 bool resolve_arguments(const int &, const char **);
+void show_environment(const environment_t &);
 
 int main(int argc, char **argv) {
 
@@ -12,6 +17,9 @@ int main(int argc, char **argv) {
             return EXIT_CODE_UNKNOWN_ARGUMENT;
         }
     }
+
+    environment_t environment;
+    show_environment(environment);
 
     return EXIT_CODE_CORRECT;
 }
@@ -27,4 +35,15 @@ bool resolve_arguments(const int &argc, const char **argv) {
         }
     }
     return true;
+}
+
+void show_environment(const environment_t &environment) {
+    cout << "==================== Processor ====================" << endl;
+    cout << "      Processor Vendor : " << environment.get_processor_vendor() << endl;
+    cout << "        Processor Name : " << environment.get_processor_name() << endl;
+    cout << "Processor Thread Count : " << environment.get_processor_threads() << endl;
+    auto processor_cache = environment.get_processor_cache();
+    cout << "    Processor Cache L1 : " << processor_cache.l1 << " KB" << endl;
+    cout << "    Processor Cache L2 : " << processor_cache.l2 << " KB" << endl;
+    cout << "    Processor Cache L3 : " << processor_cache.l3 << " KB" << endl;
 }
