d6748a23cd87462bd9f75836f86e268b7d717512
1552980358 <1552980358@qq.com>
2022-10-14 14:19:43 +0800

    winring0: Fix incorrect output file

--- winring0/winring0_dlls.cpp
+++ winring0/winring0_dlls.cpp
@@ -4,6 +4,9 @@
 #include <filesystem>
 namespace filesystem = std::filesystem;
 using filesystem::exists;
+using filesystem::path;
+#include <sstream>
+using std::stringstream;
 
 #include "cmrc/cmrc.hpp"
 CMRC_DECLARE(winring0);
@@ -20,52 +23,45 @@ using cmrc::winring0::get_filesystem;
 #define CMRC_RES_WIN_RING0_X64_SYS "lib/WinRing0x64.sys"
 
 #define DLL_FILE_OPEN_MODE "wb+"
-void write_dll_to_file(FILE *, char *, const char *);
+void write_dll_to_file(const char *, char *, const char *);
 
-void copy_win_ring0_dlls() {
-    if (!exists(WIN_RING0)) {
+void copy_win_ring0_dlls(const string &executable_dir) {
+
+    auto output_file = executable_dir + char(path::preferred_separator) + WIN_RING0;
+    if (!exists(output_file)) {
         auto win_ring0 = get_filesystem().open(CMRC_RES_WIN_RING0);
         if (win_ring0.size()) {
-            write_dll_to_file(
-                    fopen(WIN_RING0, DLL_FILE_OPEN_MODE),
-                    (char *) win_ring0.begin(),
-                    win_ring0.end()
-            );
+            write_dll_to_file(output_file.c_str(), (char *) win_ring0.begin(),win_ring0.end());
         }
     }
-    if (!exists(WIN_RING0_SYS)) {
+
+    output_file = executable_dir + char(path::preferred_separator) + WIN_RING0_SYS;
+    if (!exists(output_file)) {
         auto win_ring0 = get_filesystem().open(CMRC_RES_WIN_RING0_SYS);
         if (win_ring0.size()) {
-            write_dll_to_file(
-                    fopen(WIN_RING0_SYS, DLL_FILE_OPEN_MODE),
-                    (char *) win_ring0.begin(),
-                    win_ring0.end()
-            );
+            write_dll_to_file(output_file.c_str(), (char *) win_ring0.begin(), win_ring0.end());
         }
     }
-    if (!exists(WIN_RING0_X64)) {
+
+    output_file = executable_dir + char(path::preferred_separator) + WIN_RING0_X64;
+    if (!exists(output_file)) {
         auto win_ring0_x64 = get_filesystem().open(CMRC_RES_WIN_RING0_X64);
         if (win_ring0_x64.size()) {
-            write_dll_to_file(
-                    fopen(WIN_RING0_X64, DLL_FILE_OPEN_MODE),
-                    (char *) win_ring0_x64.begin(),
-                    win_ring0_x64.end()
-            );
+            write_dll_to_file(output_file.c_str(), (char *) win_ring0_x64.begin(), win_ring0_x64.end());
         }
     }
-    if (!exists(WIN_RING0_X64_SYS)) {
+
+    output_file = executable_dir + char(path::preferred_separator) + WIN_RING0_X64_SYS;
+    if (!exists(output_file)) {
         auto win_ring0 = get_filesystem().open(CMRC_RES_WIN_RING0_X64_SYS);
         if (win_ring0.size()) {
-            write_dll_to_file(
-                    fopen(WIN_RING0_X64_SYS, DLL_FILE_OPEN_MODE),
-                    (char *) win_ring0.begin(),
-                    win_ring0.end()
-            );
+            write_dll_to_file(output_file.c_str(), (char *) win_ring0.begin(), win_ring0.end());
         }
     }
 }
 
-void write_dll_to_file(FILE *output_file, char *file_begin, const char *file_end) {
+void write_dll_to_file(const char *output_file_path, char *file_begin, const char *file_end) {
+    auto output_file = fopen(output_file_path, DLL_FILE_OPEN_MODE);
     for (; file_begin < file_end;) {
         fputc(*file_begin++, output_file);
     }
--- winring0/winring0_dlls.h
+++ winring0/winring0_dlls.h
@@ -1,6 +1,9 @@
 #ifndef CPUSTRESS_WINRING0_DLLS_H
 #define CPUSTRESS_WINRING0_DLLS_H
 
-void copy_win_ring0_dlls();
+#include <string>
+using std::string;
+
+void copy_win_ring0_dlls(const string &executable_dir);
 
 #endif //CPUSTRESS_WINRING0_DLLS_H
