4b140916a6c9fd115a8b13816872c5fb54ec5c74
1552980358 <1552980358@qq.com>
2022-10-16 04:07:16 +0800

    Clean argument methods

--- argument/argument_init.h
+++
@@ -1,6 +0,0 @@
-#ifndef CPUSTRESS_ARGUMENT_INIT_H
-#define CPUSTRESS_ARGUMENT_INIT_H
-
-void argument_init(const string &executable_dir);
-
-#endif //CPUSTRESS_ARGUMENT_INIT_H
--- main.cpp
+++ main.cpp
@@ -6,44 +6,19 @@ namespace filesystem = std::filesystem;
 using filesystem::exists;
 using filesystem::path;
 
-#include "argument/argument_parameters.h"
-#include "argument/argument_init.h"
 #include "exit_code.h"
 
 #include "environment/environment.h"
 
-bool resolve_arguments(const string &executable_dir, const int &, const char **);
 void show_environment(const environment_t &);
 
 int main(int argc, char **argv) {
 
-    if (argc > 1) {    // has argument(s)
-        auto executable_path = string(*argv);
-        auto executable_dir = executable_path.substr(0, executable_path.find_last_of(path::preferred_separator));
-        if (!resolve_arguments(executable_dir, argc, (const char **) argv)) {
-            return EXIT_CODE_UNKNOWN_ARGUMENT;
-        }
-    }
 
-    environment_t environment;
-    show_environment(environment);
 
     return EXIT_CODE_CORRECT;
 }
 
-bool resolve_arguments(const string &executable_dir, const int &argc, const char **argv) {
-    auto count = 1;
-    while (count < argc) {
-        switch (resolve_argument(argv[count++])) {
-            case ARGUMENT_INIT:
-                argument_init(executable_dir);
-                break;
-            case ARGUMENT_UNKNOWN:
-                return false;
-        }
-    }
-    return true;
-}
 
 void show_environment(const environment_t &environment) {
     cout << "==================== Processor ====================" << endl;
