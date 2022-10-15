7b701e06109bd942d59fc2549c589abc417d797c
1552980358 <1552980358@qq.com>
2022-10-16 04:15:44 +0800

    main.cpp: Clean up

--- main.cpp
+++ main.cpp
@@ -1,20 +1,5 @@
-#include <iostream>
-using std::cout;
-using std::endl;
-#include <filesystem>
-namespace filesystem = std::filesystem;
-using filesystem::exists;
-using filesystem::path;
-
 #include "exit_code.h"
 
-#include "environment/environment.h"
-
-void show_environment(const environment_t &);
-
 int main(int argc, char **argv) {
-
-
-
     return EXIT_CODE_CORRECT;
 }
