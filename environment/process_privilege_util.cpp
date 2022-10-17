88aa7b7b284805239e87548832f81b5ecc39dbd0
1552980358 <1552980358@qq.com>
2022-10-17 09:50:28 +0800

    environment: Update process_privilege_util

---
+++ environment/process_privilege_util.cpp
@@ -0,0 +1,19 @@
+#include "process_privilege_util.h"
+
+#if defined(_WIN32) || defined(_WIN64) || defined(__CYGWIN__) || defined(__MINGW32__)
+
+#include <shlobj.h>
+
+bool check_privilege_elevated() {
+    return IsUserAnAdmin();
+}
+
+#elif defined(__linux__) || defined(linux) || defined(__linux) || defined(unix) || defined(__unix) || defined(__unix__)
+
+#include <unistd.h>
+
+bool check_privilege_elevated() {
+    return getuid() == 0 && geteuid() == 0;
+}
+
+#endif
---
+++ environment/process_privilege_util.h
@@ -0,0 +1,8 @@
+#ifndef CPUSTRESS_PROCESS_PRIVILEGE_UTIL_H
+#define CPUSTRESS_PROCESS_PRIVILEGE_UTIL_H
+
+bool check_privilege_elevated();
+
+void run_with_privilege();
+
+#endif //CPUSTRESS_PROCESS_PRIVILEGE_UTIL_H
